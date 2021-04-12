4c974c01d6f0dbfbaddd681e359c6d4754bb4009
SimiVoid <40991767+SimiVoid@users.noreply.github.com>
2021-04-12 19:05:39 +0200

    Add gui liblary and window display structure

--- Student-Life-Simulation/src/main.cpp
+++ Student-Life-Simulation/src/main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include <iomanip>
 
+#include <TGUI/TGUI.hpp>
+
 #include "Simulation.h"
 
 int main(int argc, char* argv[]) {
 	Simulation* simulation = nullptr;
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Student Life Simulator", sf::Style::Close);
+	window.setFramerateLimit(60);
 	
-	//TODO: make console 
+	tgui::GuiSFML gui(window);
+	
+	//TODO: make gui menu
 
 	//TODO: async window drawing and simulation working
 
+	while(window.isOpen()) {
+		sf::Event event;
+
+		while(window.pollEvent(event)) {
+			gui.handleEvent(event);
+
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		gui.draw();
+
+		window.display();
+	}
+	
 	delete simulation;
 	
 	return 0;
