dfe70a6108c92b2b729fb0a51c47fe1bd7154f0c
SimiVoid <40991767+SimiVoid@users.noreply.github.com>
2021-04-12 12:07:23 +0200

    Create BoardStatus class and StudentStatus enumetation class

--- Student-Life-Simulation/src/BoardStatus.cpp
+++ Student-Life-Simulation/src/BoardStatus.cpp
@@ -1,1 +1,18 @@
 #include "BoardStatus.h"
+
+BoardStatus::BoardStatus(const uint16_t studentsOnStudiesCount, const uint16_t studentsFailedCount, const uint16_t studentPassedCount)
+	:m_studentsOnStudiesCount(studentsOnStudiesCount), m_studentsFailedCount(studentsFailedCount), m_studentsPassedCount(studentPassedCount) {
+
+}
+
+uint16_t BoardStatus::getStudentsOnStudiesCount() const {
+	return m_studentsOnStudiesCount;
+}
+
+uint16_t BoardStatus::getStudentsFailedCount() const {
+	return m_studentsFailedCount;
+}
+
+uint16_t BoardStatus::getStudentsPassedCount() const {
+	return m_studentsPassedCount;
+}
--- Student-Life-Simulation/src/BoardStatus.h
+++ Student-Life-Simulation/src/BoardStatus.h
@@ -1,5 +1,18 @@
 #pragma once
-class BoardStatus
-{
+
+#include <cstdint>
+
+class BoardStatus {
+	uint16_t m_studentsOnStudiesCount;
+	uint16_t m_studentsFailedCount;
+	uint16_t m_studentsPassedCount;
+
+public:
+	explicit BoardStatus(uint16_t studentsOnStudiesCount, uint16_t studentsFailedCount, uint16_t studentPassedCount);
+	~BoardStatus() = default;
+
+	[[nodiscard]] uint16_t getStudentsOnStudiesCount() const;
+	[[nodiscard]] uint16_t getStudentsFailedCount() const;
+	[[nodiscard]] uint16_t getStudentsPassedCount() const;
 };
 
