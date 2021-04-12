8388286a414210e1b60b2aebf1be089ca6609770
SimiVoid <40991767+SimiVoid@users.noreply.github.com>
2021-04-12 11:55:16 +0200

    Add all classes files

---
+++ Student-Life-Simulation/src/Examiner.cpp
@@ -0,0 +1,1 @@
+#include "Examiner.h"
--- Student-Life-Simulation/src/main.cpp
+++ Student-Life-Simulation/src/main.cpp
@@ -1,5 +1,8 @@
-
+#ifndef _DEBUG
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
 
 int main(int argc, char* argv[]) {
+	
 	return 0;
 }
