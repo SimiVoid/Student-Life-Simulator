7acd3dff0b5ae3e17aaacbc22fe1ec7114c0ac13
SimiVoid <40991767+SimiVoid@users.noreply.github.com>
2021-04-12 17:51:39 +0200

    Small new things :)

--- Student-Life-Simulation/src/main.cpp
+++ Student-Life-Simulation/src/main.cpp
@@ -4,9 +4,13 @@
 #include "Simulation.h"
 
 int main(int argc, char* argv[]) {
+	Simulation* simulation = nullptr;
+	
 	//TODO: make console 
 
 	//TODO: async window drawing and simulation working
+
+	delete simulation;
 	
 	return 0;
 }
