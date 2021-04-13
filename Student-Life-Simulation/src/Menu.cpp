3d1bb565344b8748cb0f7eb0d3b38cd8ac56af03
SimiVoid <40991767+SimiVoid@users.noreply.github.com>
2021-04-13 13:21:09 +0200

    Working menu wihout start button

--- Student-Life-Simulation/src/Menu.cpp
+++ Student-Life-Simulation/src/Menu.cpp
@@ -12,16 +12,19 @@ void setupMenu(tgui::GuiSFML& gui, sf::RenderWindow& window, Simulation& simulat
 	auto generatePlotButton = tgui::Button::create("Generate plot");
 	generatePlotButton->setSize(tgui::Layout2d(190, 40));
 	generatePlotButton->setPosition(tgui::Layout2d(5, 505));
+	generatePlotButton->onMousePress(generatePlotButtonOnMousePress, &simulation);
 	gui.add(generatePlotButton);
 
 	auto exportDataButton = tgui::Button::create("Export Data");
 	exportDataButton->setSize(tgui::Layout2d(190, 40));
 	exportDataButton->setPosition(tgui::Layout2d(5, 455));
+	exportDataButton->onMousePress(exportDataButtonOnMousePress, &simulation);
 	gui.add(exportDataButton);
 
 	auto startStopButton = tgui::Button::create("Start");
 	startStopButton->setSize(tgui::Layout2d(190, 40));
 	startStopButton->setPosition(tgui::Layout2d(5, 5));
+	startStopButton->onMousePress(startStopButtonOnMousePress);
 	gui.add(startStopButton);
 
 	auto boardSizeSliderText = tgui::Label::create("Size: 10");
@@ -51,10 +54,42 @@ void setupMenu(tgui::GuiSFML& gui, sf::RenderWindow& window, Simulation& simulat
 	studentsCountSlider->setStep(1);
 	studentsCountSlider->onValueChange(sliderOnValueChange, studentsCountSlider, studentsCountSliderText, "Students count: ");
 	gui.add(studentsCountSlider);
+
+	auto examinersCountSliderText = tgui::Label::create("Examiners count: 1");
+	examinersCountSliderText->setSize(tgui::Layout2d(190, 20));
+	examinersCountSliderText->setPosition(tgui::Layout2d(5, 275));
+	examinersCountSliderText->setVerticalAlignment(tgui::Label::VerticalAlignment::Center);
+	examinersCountSliderText->setHorizontalAlignment(tgui::Label::HorizontalAlignment::Center);
+	gui.add(examinersCountSliderText);
+
+	auto examinersCountSlider = tgui::Slider::create(1, 100);
+	examinersCountSlider->setSize(tgui::Layout2d(170, 20));
+	examinersCountSlider->setPosition((tgui::Layout2d(15, 315)));
+	examinersCountSlider->setStep(1);
+	examinersCountSlider->onValueChange(sliderOnValueChange, examinersCountSlider, examinersCountSliderText, "Examiners count: ");
+	gui.add(examinersCountSlider);
 }
 
 void exitButtonOnMousePress(sf::RenderWindow& window) {
 	window.close();
+}
+
+void generatePlotButtonOnMousePress(Simulation* simulation) {
+	if (simulation != nullptr)
+		simulation->generatePlot();
+
+	//TODO: on error msg
+}
+
+void exportDataButtonOnMousePress(Simulation* simulation) {
+	if (simulation != nullptr)
+		simulation->exportData();
+
+	//TODO: on error msg
+}
+
+void startStopButtonOnMousePress() {
+	
 }
 
 void sliderOnValueChange(std::shared_ptr<tgui::Slider> slider, std::shared_ptr<tgui::Label> sliderText, const std::string& text) {
--- Student-Life-Simulation/src/Menu.h
+++ Student-Life-Simulation/src/Menu.h
@@ -9,4 +9,7 @@
 void setupMenu(tgui::GuiSFML& gui, sf::RenderWindow& window, Simulation& simulation);
 
 void exitButtonOnMousePress(sf::RenderWindow& window);
+void startStopButtonOnMousePress();
+void generatePlotButtonOnMousePress(Simulation* simulation);
+void exportDataButtonOnMousePress(Simulation* simulation);
 void sliderOnValueChange(std::shared_ptr<tgui::Slider> slider, std::shared_ptr<tgui::Label> sliderText, const std::string& text);
