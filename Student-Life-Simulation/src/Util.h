9896b3bdcae95c8a6671c5f34d2f16b7641ad12f
SimiVoid <40991767+SimiVoid@users.noreply.github.com>
2021-04-14 00:46:37 +0200

    Working random number generator

--- Student-Life-Simulation/src/Util.h
+++ Student-Life-Simulation/src/Util.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <utility>
+#include <random>
 
 template<typename T>
-T randomNumberWithinRange(std::pair<T, T> range);
+T randomNumberWithinRange(std::pair<T, T> range) {
+	std::random_device randomDevice;
+	std::mt19937 generator(randomDevice());
+	const std::uniform_real_distribution<double> distribution(range.first, range.second);
+
+	return static_cast<T>(distribution(generator));
+}
