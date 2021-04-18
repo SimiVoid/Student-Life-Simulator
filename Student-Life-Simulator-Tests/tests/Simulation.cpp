845bf9461caf75a2c90ace6646cd504c7c093663
SimiVoid <40991767+SimiVoid@users.noreply.github.com>
2021-04-18 19:49:16 +0200

    Rename project xd

--- Student-Life-Simulator-Tests/tests/Simulation.cpp
+++ Student-Life-Simulator-Tests/tests/Simulation.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
-#include "../../Student-Life-Simulator/src/Simulation.h"
-TEST(TestCaseName, d) {
+#include "../../S"
+TEST(TestCaseName, Simulation) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
 }
