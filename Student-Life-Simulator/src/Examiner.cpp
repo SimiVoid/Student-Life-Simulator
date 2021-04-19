21acaaeb9982d91ba350a667c0b8bc730fc0ae78
xNombre <kartapolska@gmail.com>
2021-04-19 23:16:01 +0200

    Examiner: Fill in examine student method

--- Student-Life-Simulator/src/Examiner.cpp
+++ Student-Life-Simulator/src/Examiner.cpp
@@ -1,4 +1,5 @@
 #include "Examiner.h"
+#include "Util.h"
 
 Examiner::Examiner(const std::pair<uint16_t, uint16_t> suspicionRange)
 	: Agent() {
@@ -10,5 +11,23 @@ uint16_t Examiner::getSuspicion() const {
 }
 
 void Examiner::examinateStudent(Student* student) {
-	// TODO: examinate student alorithm
+	// Examiners hate sleeping students...
+	if (student->isSleeping()) {
+		student->acceptExamResult(false);
+	}
+	// Sober student always passes an exam
+	else if (student->getIntoxication() == 0) {
+		student->acceptExamResult(true);
+	}
+	// Student is drunk
+	// Choose a random number between 1 and 100
+	// Compare it to m_suspicion which acts as
+	// a likeliness of failing an exam
+	else if (randomNumberWithinRange(std::make_pair(1, 100)) <= m_suspicion) {
+		student->acceptExamResult(false);
+	}
+
+	// The chance of drunk student passing an
+	// exam is (100 - m_suspicion) %
+	student->acceptExamResult(true);
 }
