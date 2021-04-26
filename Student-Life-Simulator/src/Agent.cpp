SimiVoid/Student-Life-Simulator

commit b2c08b09fecc4dece7ee39bd211bcd8dac737e03

Fix board not filling the whole render window
Using integer here caused miscalculation and board wasn't filling its whole window - there were black borders on the right and bottom.

Student-Life-Simulator/src/Agent.cpp
@@ -19,13 +19,13 @@ uint16_t Agent::getId() const {
 	return m_id;
 }
 
-void Agent::draw(sf::RenderWindow& window, const uint16_t& fieldSize, const bool& isSingle) {
+void Agent::draw(sf::RenderWindow& window, const float& fieldSize, const bool& isSingle) {
 	if (isSingle) {
-		m_singleView.setPosition(200 + fieldSize * m_position.x, fieldSize * m_position.y);
+		m_singleView.setPosition(200.f + fieldSize * m_position.x, fieldSize * m_position.y);
 		window.draw(m_singleView);
 	}
 	else {
-		m_multipleView.setPosition(200 + fieldSize * m_position.x, fieldSize * m_position.y);
+		m_multipleView.setPosition(200.f + fieldSize * m_position.x, fieldSize * m_position.y);
 		window.draw(m_multipleView);
 	}
 }

Student-Life-Simulator/src/Agent.h
@@ -22,6 +22,6 @@ public:
 
 	[[nodiscard]] sf::Vector2i getPosition() const;
 	[[nodiscard]] uint16_t getId() const;
-	void draw(sf::RenderWindow& window, const uint16_t& fieldSize, const bool& isSingle = true);
+	void draw(sf::RenderWindow& window, const float& fieldSize, const bool& isSingle = true);
 	virtual void move(const uint16_t& boardSize);
 };

Student-Life-Simulator/src/BoardField.cpp
@@ -36,7 +36,7 @@ void BoardField::draw(sf::RenderWindow& window, const uint16_t boardSize) {
 	uint16_t studentsCount = 0;
 	uint16_t examinersCount = 0;
 
-	const auto fieldSize = floorf(static_cast<float>(window.getSize().y) / static_cast<float>(boardSize));
+	const float fieldSize = (window.getSize().y * 1.f) / (boardSize);
 	
 	for (const auto& agent : m_agentsOnField) {
 		if (castAgentTo<Student>(agent))
@@ -53,7 +53,7 @@ void BoardField::draw(sf::RenderWindow& window, const uint16_t boardSize) {
 	}
 
 	m_border.setSize(sf::Vector2f(fieldSize, fieldSize));
-	m_border.setPosition(200.f + fieldSize * m_position.x, fieldSize * m_position.y);
+	m_border.setPosition(200.f + fieldSize * (m_position.x), fieldSize * (m_position.y));
 
 	window.draw(m_border);
 }

commit c63eb33fa1b190726d1f0ac0aeab433a11688f36

Rewrite drawing algorithm (once again)
Commit b2c08b09fecc4dece7ee39bd211bcd8dac737e03 introduced yet another bug that

Student-Life-Simulator/src/Agent.cpp
@@ -1,15 +1,11 @@
 #include "Agent.h"
 #include "Util.h"
 
-#include <random>
-
 uint16_t Agent::idCounter = 0;
 
 Agent::Agent(const uint16_t& boardSize)
-	:m_id(idCounter++) {
-	m_position = sf::Vector2i(randomNumberWithinRange<uint16_t>(0, boardSize - 1),
-		randomNumberWithinRange<uint16_t>(0, boardSize - 1));	
-}
+	: m_id(idCounter++)
+	, m_position(randomNumberWithinRange(0, boardSize - 1), randomNumberWithinRange(0, boardSize - 1)) {}
 
 sf::Vector2i Agent::getPosition() const {
 	return m_position;
@@ -19,15 +15,11 @@ uint16_t Agent::getId() const {
 	return m_id;
 }
 
-void Agent::draw(sf::RenderWindow& window, const float& fieldSize, const bool& isSingle) {
-	if (isSingle) {
-		m_singleView.setPosition(200 + fieldSize * m_position.x, fieldSize * m_position.y);
+void Agent::draw(sf::RenderWindow& window, const bool isSingle) {
+	if (isSingle)
 		window.draw(m_singleView);
-	}
-	else {
-		m_multipleView.setPosition(200 + fieldSize * m_position.x, fieldSize * m_position.y);
+	else
 		window.draw(m_multipleView);
-	}
 }
 
 void Agent::move(const uint16_t& boardSize) {
@@ -85,4 +77,9 @@ void Agent::move(const uint16_t& boardSize) {
 	} while (newPosition.x < 0 || newPosition.y < 0 || newPosition.x >= boardSize || newPosition.y >= boardSize);
 
 	m_position = newPosition;
+
+	const float fieldSize = static_cast<float>(1000 / boardSize);
+	const float offset = static_cast<float>(1000 % boardSize / 2);
+	m_singleView.setPosition(200 + fieldSize * m_position.x + offset, fieldSize * m_position.y + offset);
+	m_multipleView.setPosition(200 + fieldSize * m_position.x + offset, fieldSize * m_position.y + offset);
 }

Student-Life-Simulator/src/Agent.h
@@ -7,10 +7,7 @@ class Agent {
 protected:
 	uint16_t m_id;
 	sf::Vector2i m_position;
-	/**
-	 * Green - Student
-	 * Brown - Examiner
-	 */
+
 	sf::RectangleShape m_singleView;
 	sf::ConvexShape m_multipleView;
 
@@ -22,6 +19,6 @@ public:
 
 	[[nodiscard]] sf::Vector2i getPosition() const;
 	[[nodiscard]] uint16_t getId() const;
-	void draw(sf::RenderWindow& window, const float& fieldSize, const bool& isSingle = true);
+	void draw(sf::RenderWindow& window, const bool isSingle = true);
 	virtual void move(const uint16_t& boardSize);
 };

Student-Life-Simulator/src/Board.cpp
@@ -2,7 +2,10 @@
 
 Board::Board(const uint16_t size)
 	:m_size(size) {
-	const float fieldSize = (1000 * 1.f) / (m_size);
+	const float fieldSize = static_cast<float>(1000 / m_size);
+	const float offset = static_cast<float>(1000 % m_size / 2);
+
+	m_boardGrid.setPrimitiveType(sf::Lines);
 
 	for (uint16_t x = 0; x < size; x++) {
 		std::vector<BoardField> column;
@@ -10,15 +13,18 @@ Board::Board(const uint16_t size)
 			column.emplace_back(BoardField(sf::Vector2i(x, y)));
 		}
 		m_fields.emplace_back(column);
-		
-		m_boardGrid.setPrimitiveType(sf::Lines);
-		
-		// x offset is 200, board size is 1000x1000
-		m_boardGrid.append(sf::Vertex(sf::Vector2f(200, x * fieldSize), m_gridColor));
-		m_boardGrid.append(sf::Vertex(sf::Vector2f(1200, x * fieldSize), m_gridColor));
-		m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + x * fieldSize, 0), m_gridColor));
-		m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + x * fieldSize , 1000), m_gridColor));
+
+		// x offset is 200
+		m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + offset, x * fieldSize + offset), m_gridColor));
+		m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + fieldSize * size + offset, x * fieldSize + offset), m_gridColor));
+		m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + x * fieldSize + offset, offset), m_gridColor));
+		m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + x * fieldSize + offset, fieldSize * size + offset), m_gridColor));
 	}
+
+	m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + offset, size * fieldSize + offset), m_gridColor));
+	m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + fieldSize * size + offset, size * fieldSize + offset), m_gridColor));
+	m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + size * fieldSize + offset, 0 + offset), m_gridColor));
+	m_boardGrid.append(sf::Vertex(sf::Vector2f(200 + size * fieldSize + offset, fieldSize * size + offset), m_gridColor));
 }
 
 void Board::checkFieldPosition(const sf::Vector2i& position) const {
@@ -44,11 +50,12 @@ uint16_t Board::getBoardSize() const {
 }
 
 void Board::draw(sf::RenderWindow& window) {
-	window.draw(m_boardGrid);
+
 
 	for (auto& columns : m_fields)
 		for (auto& field : columns)
-			field.draw(window, m_size);
+			field.draw(window);
+	window.draw(m_boardGrid);
 }
 
 void Board::updateField(const sf::Vector2i& position, const std::set<std::shared_ptr<Agent>>& agents) {

Student-Life-Simulator/src/Board.h
@@ -5,7 +5,7 @@
 typedef std::vector<std::vector<BoardField>> BoardArray;
 
 class Board {
-	const sf::Color& m_gridColor = sf::Color(31, 31, 31);
+	const sf::Color& m_gridColor = sf::Color(41, 41, 41);;
 	sf::VertexArray m_boardGrid;
 
 	BoardArray m_fields;

Student-Life-Simulator/src/BoardField.cpp
@@ -28,11 +28,9 @@ void BoardField::clearField() {
 	m_agentsOnField.clear();
 }
 
-void BoardField::draw(sf::RenderWindow& window, const uint16_t& boardSize) {
+void BoardField::draw(sf::RenderWindow& window) {
 	std::shared_ptr<Student> highestPrioStudent = nullptr;
 	std::shared_ptr<Examiner> examinerInField = nullptr;
-
-	const float fieldSize = 1000.f / boardSize;
 	
 	for (const auto& agent : m_agentsOnField) {
 		if (isAgentTypeof<Student>(agent)) {
@@ -50,13 +48,13 @@ void BoardField::draw(sf::RenderWindow& window, const uint16_t& boardSize) {
 	}
 	
 	if (highestPrioStudent && examinerInField) {
-		highestPrioStudent->draw(window, fieldSize, false);
-		examinerInField->draw(window, fieldSize, false);
+		highestPrioStudent->draw(window, false);
+		examinerInField->draw(window, false);
 	}
 	else {
 		if (highestPrioStudent)
-			highestPrioStudent->draw(window, fieldSize);
+			highestPrioStudent->draw(window);
 		else if(examinerInField)
-			examinerInField->draw(window, fieldSize);
+			examinerInField->draw(window);
 	}
 }

Student-Life-Simulator/src/BoardField.h
@@ -18,5 +18,5 @@ public:
 	void setAgents(std::set<std::shared_ptr<Agent>> agents);
 	void addAgent(const std::shared_ptr<Agent>& agent);
 	void clearField();
-	void draw(sf::RenderWindow& window, const uint16_t& boardSize);
+	void draw(sf::RenderWindow& window);
 };

Student-Life-Simulator/src/Examiner.cpp
@@ -5,8 +5,7 @@ Examiner::Examiner(const std::pair<uint16_t, uint16_t>& suspicionRange, const ui
 	: Agent(boardSize) {
 	m_suspicion = randomNumberWithinRange(suspicionRange);
 
-	const unsigned windowHeight = 1000;
-	const auto fieldSize = floorf(static_cast<float>(windowHeight) / static_cast<float>(boardSize));
+	const float fieldSize = static_cast<float>(1000 / boardSize - 1);
 
 	m_singleView.setSize({ fieldSize, fieldSize });
 	m_singleView.setFillColor(sf::Color(165, 42, 42));
@@ -16,7 +15,6 @@ Examiner::Examiner(const std::pair<uint16_t, uint16_t>& suspicionRange, const ui
 	m_multipleView.setPoint(1, { fieldSize, 0 });
 	m_multipleView.setPoint(2, { 0, fieldSize });
 	m_multipleView.setFillColor(sf::Color(165, 42, 42));
-
 }
 
 uint16_t Examiner::getSuspicion() const {

Student-Life-Simulator/src/Student.cpp
@@ -5,8 +5,7 @@ Student::Student(const std::pair<uint16_t, uint16_t>& knowledgeRange, const std:
 	m_knowledge = randomNumberWithinRange(knowledgeRange);
 	m_alcoholResistance = randomNumberWithinRange(alcoholResistanceRange);
 
-	const unsigned windowHeight = 1000;
-	const auto fieldSize = floorf(static_cast<float>(windowHeight) / static_cast<float>(boardSize));
+	const float fieldSize = static_cast<float>(1000 / boardSize - 1);
 
 	m_singleView.setSize({ fieldSize, fieldSize });
 	m_singleView.setFillColor(sf::Color::Green);