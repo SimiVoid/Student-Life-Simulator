ff1443f3a662575427e498a705b306109a733fd0
xNombre <kartapolska@gmail.com>
2021-04-27 20:56:45 +0200

    Add more Board tests
    Add test for commit:
    0084b9c84bfe72585ae9cd780d18cc9e3c62bddf

--- Student-Life-Simulator-Tests/tests/Board.cpp
+++ Student-Life-Simulator-Tests/tests/Board.cpp
@@ -4,8 +4,35 @@
 
 TEST(Board, BoardGetSize) {
 	const uint16_t boardSize = 10;
-	
 	Board board(boardSize);
 
 	ASSERT_EQ(board.getBoardSize(), boardSize);
+}
+
+TEST(Board, BoardFieldsRange) {
+	const uint16_t boardSize = 10;
+	Board board(boardSize);
+
+	ASSERT_NO_THROW(board.getField({ 0,0 }));
+	ASSERT_NO_THROW(board.getField({ 9, 9 }));
+
+	ASSERT_THROW(board.getField({ 10, 10 }), std::out_of_range);
+	ASSERT_THROW(board.getField({ 0, 10 }), std::out_of_range);
+	ASSERT_THROW(board.getField({ 10, 0 }), std::out_of_range);
+
+	ASSERT_THROW(board.getField({ -1, -1 }), std::out_of_range);
+	ASSERT_THROW(board.getField({ 0, -1 }), std::out_of_range);
+	ASSERT_THROW(board.getField({ -1, 0 }), std::out_of_range);
+}
+
+TEST(Board, BoardGetAndSetField) {
+	const uint16_t boardSize = 10;
+	Board board(boardSize);
+
+	std::shared_ptr<Agent> agent;
+	agent = std::make_shared<Agent>(boardSize);
+	std::set<std::shared_ptr<Agent>> set{ agent };
+
+	ASSERT_NO_THROW(board.getField({ 0,0 }).addAgent(agent));
+	ASSERT_EQ(board.getField({ 0,0 }).getAgents(), set);
 }
