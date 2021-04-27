b9d235b31b03d5d17d9baf25a30fa0f6f60f0784
xNombre <kartapolska@gmail.com>
2021-04-27 15:17:28 +0200

    Add simple examination tests

--- Student-Life-Simulator-Tests/tests/Examiner.cpp
+++ Student-Life-Simulator-Tests/tests/Examiner.cpp
@@ -1,9 +1,33 @@
 #include "gtest/gtest.h"
 
 #include "../src/Examiner.h"
+#include "../src/Student.h"
 
 TEST(Examiner, ExaminerClassTest) {
 	Examiner examiner(std::make_pair(1, 1), 1);
 	
 	ASSERT_EQ(examiner.getSuspicion(), 1);	
+}
+
+TEST(Examiner, ExaminerPassSober) {
+	Examiner examiner(std::make_pair(100, 100), 1);
+	std::shared_ptr<Student> student;
+	student = std::make_shared<Student>(std::make_pair(1, 1), std::make_pair(1, 1), 1);
+
+	examiner.examinateStudent(student);
+
+	ASSERT_EQ(student->getPassedExams(), 1);
+	ASSERT_EQ(student->getFailedExams(), 0);
+}
+
+TEST(Examiner, ExaminerFailDrunk) {
+	Examiner examiner(std::make_pair(100, 100), 1);
+	std::shared_ptr<Student> student;
+	student = std::make_shared<Student>(std::make_pair(1, 1), std::make_pair(1, 1), 1);
+
+	student->drinkBeer();
+	examiner.examinateStudent(student);
+
+	ASSERT_EQ(student->getPassedExams(), 0);
+	ASSERT_EQ(student->getFailedExams(), 1);
 }
