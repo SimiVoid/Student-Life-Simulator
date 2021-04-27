61c9469b20c39ff30cc43396faacf01db5a5bb28
xNombre <kartapolska@gmail.com>
2021-04-27 20:59:28 +0200

    Add Examiner test for sleeping student

--- Student-Life-Simulator-Tests/tests/Examiner.cpp
+++ Student-Life-Simulator-Tests/tests/Examiner.cpp
@@ -31,3 +31,16 @@ TEST(Examiner, ExaminerFailDrunk) {
 	ASSERT_EQ(student->getPassedExams(), 0);
 	ASSERT_EQ(student->getFailedExams(), 1);
 }
+
+TEST(Examiner, ExaminerFailSleeping) {
+	Examiner examiner(std::make_pair(100, 100), 1);
+	std::shared_ptr<Student> student;
+	student = std::make_shared<Student>(std::make_pair(1, 1), std::make_pair(1, 1), 1);
+
+	student->drinkBeer();
+	student->drinkBeer();
+	examiner.examinateStudent(student);
+
+	ASSERT_EQ(student->getPassedExams(), 0);
+	ASSERT_EQ(student->getFailedExams(), 1);
+}
