d1232b913dab7d07e1bf32447dba26b2e3baecf0
xNombre <kartapolska@gmail.com>
2021-04-27 20:39:10 +0200

    BoardField: Add SetAndClearAgents test

--- Student-Life-Simulator-Tests/tests/BoardField.cpp
+++ Student-Life-Simulator-Tests/tests/BoardField.cpp
@@ -19,4 +19,28 @@ TEST(BoardField, SetAndReadAgents) {
 	const auto agentsOut = boardField.getAgents();
 
 	ASSERT_EQ(agentsIn, agentsOut);
+
+	boardField.clearField();
+}
+
+TEST(BoardField, SetAndClearAgents) {
+	BoardField boardField;
+
+	std::set<std::shared_ptr<Agent>> emptySet = {};
+	std::set<std::shared_ptr<Agent>> agentsIn = {
+		std::make_shared<Agent>(Agent(10)),
+		std::make_shared<Agent>(Agent(10)),
+		std::make_shared<Agent>(Agent(10)),
+		std::make_shared<Agent>(Agent(10)),
+		std::make_shared<Agent>(Agent(10))
+	};
+
+	for (auto& agent : agentsIn)
+		boardField.addAgent(agent);
+
+	boardField.clearField();
+
+	const auto agentsOut = boardField.getAgents();
+
+	ASSERT_EQ(agentsOut, emptySet);
 }
